8d25227bc7a5e048b8830ec04a1b414dcd6aa6b8
Vedat Zeybek <vedatzeybek20@gmail.com>
2025-08-14 00:13:28 +0300

    tokenizor to parser implemented

--- includes/parser.h
+++ includes/parser.h
@@ -10,6 +10,16 @@ typedef struct s_command
 	struct s_command	*next; 	// Pointer to the next command in the pipeline
 }	t_command;
 
+typedef struct s_redir {
+	t_token_type	type;      // TOKEN_REDIRECT_OUT, TOKEN_APPEND, vb.
+	char			*filename; // tmp->next->value
+	struct s_redir	*next;
+}	t_redir;
 
+
+void	free_command(t_command *cmd);
+t_command	*create_command(char **argv, char **redirections);
+void	add_command(t_command **head, t_command *new_cmd);
+t_command	*parse_command(t_token **tokens);
 
 #endif
--- srcs/parser/parser1.c
+++ srcs/parser/parser1.c
@@ -1,41 +1,94 @@
 #include "../../includes/parser.h"
 
-t_command *create_command(char **argv, char **redirections)
+int	add_to_argv(char ***argv, char *value)
 {
-	t_command *cmd;
+	char	**new_argv;
+	int		i;
 
-	cmd = malloc(sizeof(t_command));
-	if (!cmd)
+	if (!argv || !value)
+		return (0);
+	if (!*argv)
+	{
+		*argv = malloc(2 * sizeof(char *));
+		if (!*argv)
+			return (0);
+		(*argv)[0] = ft_strdup(value);
+		(*argv)[1] = NULL;
+		return (1);
+	}
+	i = 0;
+	while ((*argv)[i])
+		i++;
+	new_argv = realloc(*argv, (i + 2) * sizeof(char *));
+	if (!new_argv)
+		return (0);
+	*argv = new_argv;
+	(*argv)[i] = ft_strdup(value);
+	(*argv)[i + 1] = NULL;
+	return (1);
+}
+
+int	add_to_redirections(t_redir **list, t_token_type type, char *filename)
+{
+	t_redir	*new_node;
+	t_redir	*tmp;
+
+	new_node = malloc(sizeof(t_redir));
+	if (!new_node)
+		return (0);
+	new_node->type = type;
+	new_node->filename = ft_strdup(filename);
+	new_node->next = NULL;
+	if (!*list)
+		*list = new_node;
+	else
+	{
+		tmp = *list;
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = new_node;
+	}
+	return (1);
+}
+
+static void	parse_token_chunk(t_token **tmp, char ***argv, t_redir **redirs)
+{
+	if ((*tmp)->type == TOKEN_WORD)
+		add_to_argv(argv, (*tmp)->value);
+	else if ((*tmp)->type == TOKEN_REDIRECT_IN
+		|| (*tmp)->type == TOKEN_REDIRECT_OUT
+		|| (*tmp)->type == TOKEN_APPEND
+		|| (*tmp)->type == TOKEN_HEREDOC)
+	{
+		if ((*tmp)->next)
+		{
+			add_to_redirections(redirs, (*tmp)->type, (*tmp)->next->value);
+			*tmp = (*tmp)->next;
+		}
+	}
+}
+
+t_command	*parse_command(t_token **tokens)
+{
+	t_token		*tmp;
+	t_command	*cmd;
+	char		**argv;
+	t_redir		*redirs;
+
+	if (!tokens || !*tokens)
 		return (NULL);
-	cmd->argv = argv;
-	cmd->redirections = redirections;
-	cmd->next = NULL;
+	tmp = *tokens;
+	argv = NULL;
+	redirs = NULL;
+	while (tmp && tmp->type != TOKEN_PIPE)
+	{
+		parse_token_chunk(&tmp, &argv, &redirs);
+		tmp = tmp->next;
+	}
+	cmd = create_command(argv, redirs);
+	if (tmp && tmp->type == TOKEN_PIPE && tmp->next)
+		cmd->next = parse_command(&tmp->next);
+	else
+		cmd->next = NULL;
 	return (cmd);
 }
-
-void add_command(t_command **head, t_command *new_cmd)
-{
-	t_command *current;
-
-	if (!head || !new_cmd)
-		return ;
-	if (!*head)
-	{
-		*head = new_cmd;
-		return ;
-	}
-	current = *head;
-	while (current->next)
-		current = current->next;
-	current->next = new_cmd;
-}
-
-void free_command(t_command *cmd)
-{
-	if (!cmd)
-		return ;
-	free_splitted(cmd->argv);
-	free_splitted(cmd->redirections);
-	free(cmd);
-}
-

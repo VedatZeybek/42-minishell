d1db971055076049cdfbf9bb1d3e289c279500a3
Vedat Zeybek <vedatzeybek20@gmail.com>
2025-08-15 13:20:40 +0300

    $ expansion added

--- srcs/expander/expander1.c
+++ srcs/expander/expander1.c
@@ -21,36 +21,33 @@
 
 char	*remove_quotes(const char *str);
 
-
-
 void	expand_command(t_command *command)
 {
 	int		i;
 	char	*no_quotes;
+	char	**splitted;
 
 	if (!command || !command->argv)
-		return;
+		return ;
 	i = 0;
 	while (command)
 	{
 		while (command->argv[i])
 		{
-			if (command->argv)
+			if (command->argv[i])
 			{
 
 			}
 		}
 	}
-	
 }
-
 
 char	*remove_quotes(const char *str)
 {
-	int	len;
 	char	*res;
-	int i;
-	int	j;
+	int		len;
+	int		i;
+	int		j;
 
 	i = 0;
 	j = 0;
@@ -66,5 +63,39 @@ char	*remove_quotes(const char *str)
 		i++;
 	}
 	res[j] = '\0';
-	return res;
+	return (res);
 }
+
+char	*expand_env_var(char *str, char **envp)
+{
+	char	*var;
+	int		i;
+
+	i = 0;
+	if (str[0] != '$')
+		return (ft_strdup(str));
+	var = str + 1;
+	while (envp[i])
+	{
+		if (ft_strncmp(envp[i], var, ft_strlen(var)) == 0
+			&& envp[i][ft_strlen(var)] == '=')
+			return (ft_strdup(envp[i] + strlen(var) + 1));
+		i++;
+	}
+	return (ft_strdup("")); 
+}
+
+//// $VAR'ı çevirmek için
+//char *expand_env_var(const char *str, char **envp)
+//{
+//    if (str[0] != '$')
+//        return strdup(str);
+
+//    const char *var = str + 1; // $ işaretinden sonrası
+//    for (int i = 0; envp[i]; i++)
+//    {
+//        if (strncmp(envp[i], var, strlen(var)) == 0 && envp[i][strlen(var)] == '=')
+//            return strdup(envp[i] + strlen(var) + 1);
+//    }
+//    return strdup(""); // bulunamazsa boş string
+//}

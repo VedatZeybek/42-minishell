f59dd1a5758e2bb12bfd92593aa7c310616d1fd8
Ertugrul Lenovo Ubuntu Terminal <ertu.pakdamar@gmail.com>
2025-08-15 09:57:55 +0300

    builtin.h norm

--- includes/builtin.h
+++ includes/builtin.h
@@ -6,15 +6,15 @@
 /*   By: epakdama <epakdama@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/14 20:46:11 by epakdama          #+#    #+#             */
-/*   Updated: 2025/08/14 20:46:47 by epakdama         ###   ########.fr       */
+/*   Updated: 2025/08/15 09:53:56 by epakdama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTIN_H
 # define BUILTIN_H
 
-#include "minishell.h"
+# include "minishell.h"
 
-int		ft_cd(char **args);
+int	ft_cd(char **args);
 
 #endif

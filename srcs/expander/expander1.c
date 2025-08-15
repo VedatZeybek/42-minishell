ccb816f58adf5d4ee292cfcd70f3bfce09db9516
Vedat Zeybek <vedatzeybek20@gmail.com>
2025-08-15 13:01:39 +0300

    remove quotes

--- srcs/expander/expander1.c
+++ srcs/expander/expander1.c
@@ -19,15 +19,52 @@
 // It should handle environment variables, quotes, and other expansions as needed.
 
 
+char	*remove_quotes(const char *str);
+
+
+
 void	expand_command(t_command *command)
 {
 	int		i;
+	char	*no_quotes;
 
 	if (!command || !command->argv)
 		return;
 	i = 0;
-	while (command->argv[i])
+	while (command)
 	{
+		while (command->argv[i])
+		{
+			if (command->argv)
+			{
 
+			}
+		}
 	}
+	
+}
+
+
+char	*remove_quotes(const char *str)
+{
+	int	len;
+	char	*res;
+	int i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	len = strlen(str);
+	res = malloc(len + 1);
+	while (str[i])
+	{
+		if (str[i] != '"' && str[i] != '\'')
+		{
+			res[j] = str[i];
+			j++;
+		}
+		i++;
+	}
+	res[j] = '\0';
+	return res;
 }
